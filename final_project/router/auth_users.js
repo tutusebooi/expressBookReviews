972c67574bbbebe10d788c69cd37cc4eef47cbbf
xolela booi <xbooi023@student.wethinkcode.co.za>
2026-04-26 22:13:42 +0200

    task 1-9 complete

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,15 +15,48 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  let user = users.find(u => u.username === username && u.password === password);
+
+  if (!user) {
+    return res.status(403).json({ message: "Invalid login" });
+  }
+
+  let token = jwt.sign({ username }, "fingerprint_customer", { expiresIn: "1h" });
+
+  req.session.authorization = {
+    accessToken: token,
+    username: username
+  };
+
+  return res.status(200).json({ message: "Login successful", token });
 });
 
+
 // Add a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+regd_users.put("/auth/review/:isbn", (req,res) => {
+  const username = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  const review = req.query.review;
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({ message: "Review added/updated" });
 });
+
+regd_users.delete("/auth/review/:isbn", (req,res) => {
+  const username = req.session.authorization.username;
+  const isbn = req.params.isbn;
+
+  if (books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res.status(200).json({ message: "Review deleted" });
+  }
+
+  return res.status(404).json({ message: "Review not found" });
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
