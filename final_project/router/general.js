b287b46fa34d12f6086e3fc4bb221c858ca24f34
xolela booi <xbooi023@student.wethinkcode.co.za>
2026-05-10 22:20:31 +0200

    Updated general.js with Axios async await methods

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,69 +5,124 @@ let books = require("./booksdb.js");
 
 const public_users = express.Router();
 
-public_users.get('/', function (req, res) {
-    return res.status(300).json(books);
+/*
+Task 1
+Get the book list available in the shop
+*/
+public_users.get('/', async function (req, res) {
+
+    try {
+
+        const response = await axios.get('http://localhost:5000/');
+
+        return res.status(200).json(response.data);
+
+    } catch (error) {
+
+        return res.status(500).json({
+            message: "Error retrieving books"
+        });
+    }
 });
 
-// Get book by ISBN using Axios
+/*
+Task 2
+Get book details based on ISBN
+*/
 public_users.get('/isbn/:isbn', async function (req, res) {
+
     const isbn = req.params.isbn;
 
     try {
+
         const response = await axios.get('http://localhost:5000/');
+
         const booksData = response.data;
 
         if (booksData[isbn]) {
+
             return res.status(200).json(booksData[isbn]);
-        } else {
-            return res.status(404).json({ message: "Book not found" });
         }
+
+        return res.status(404).json({
+            message: "Book not found"
+        });
+
     } catch (error) {
-        return res.status(500).json({ message: "Error retrieving book" });
+
+        return res.status(500).json({
+            message: "Error retrieving book"
+        });
     }
 });
 
-// Get books by author using Axios
+/*
+Task 3
+Get book details based on author
+*/
 public_users.get('/author/:author', async function (req, res) {
-    const author = req.params.author;
+
+    const author = req.params.author.toLowerCase();
 
     try {
-        const response = await axios.get('http://localhost:5000/');
-        const booksData = response.data;
 
-        const filteredBooks = Object.values(booksData).filter(
-            (book) => book.author.toLowerCase() === author.toLowerCase()
+        const response = await axios.get('http://localhost:5000/');
+
+        const booksData = Object.values(response.data);
+
+        const filteredBooks = booksData.filter(
+            (book) => book.author.toLowerCase() === author
         );
 
         if (filteredBooks.length > 0) {
+
             return res.status(200).json(filteredBooks);
-        } else {
-            return res.status(404).json({ message: "No books found" });
         }
+
+        return res.status(404).json({
+            message: "Author not found"
+        });
+
     } catch (error) {
-        return res.status(500).json({ message: "Error retrieving books" });
+
+        return res.status(500).json({
+            message: "Error retrieving books"
+        });
     }
 });
 
-// Get books by title using Axios
+/*
+Task 4
+Get book details based on title
+*/
 public_users.get('/title/:title', async function (req, res) {
-    const title = req.params.title;
+
+    const title = req.params.title.toLowerCase();
 
     try {
-        const response = await axios.get('http://localhost:5000/');
-        const booksData = response.data;
 
-        const filteredBooks = Object.values(booksData).filter(
-            (book) => book.title.toLowerCase() === title.toLowerCase()
+        const response = await axios.get('http://localhost:5000/');
+
+        const booksData = Object.values(response.data);
+
+        const filteredBooks = booksData.filter(
+            (book) => book.title.toLowerCase() === title
         );
 
         if (filteredBooks.length > 0) {
+
             return res.status(200).json(filteredBooks);
-        } else {
-            return res.status(404).json({ message: "No books found" });
         }
+
+        return res.status(404).json({
+            message: "Title not found"
+        });
+
     } catch (error) {
-        return res.status(500).json({ message: "Error retrieving books" });
+
+        return res.status(500).json({
+            message: "Error retrieving books"
+        });
     }
 });
 
